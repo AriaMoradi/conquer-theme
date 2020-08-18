a76b62a8af099472b3b0dcd6cf7a6751b4358483
Aria Moradi <aria.moradi007@gmail.com>
2020-08-18 20:57:53 +0430

    navbar done

---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => (
+    <nav>
+        <li>Homepage</li>
+        <li>About us</li>
+        <li>Services</li>
+        <li>Contact</li>
+        <li>External</li>
+    </nav>
+)
+
+
+export default Navbar;
